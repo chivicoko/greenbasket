859bdb98b6cfe540943b7c319da40eeba3160590
chivicoko <victor.c.okoye@gmail.com>
2025-02-09 03:11:35 +0100

    update dummyjson page

--- src/app/dummyjson-products/page.tsx
+++ src/app/dummyjson-products/page.tsx
@@ -1,15 +1,15 @@
 'use client';
 
 import ProductsFromDummyJson from '@/components/ProductsFromDummyJson'
-import { useUserForm } from '@/context/UserFormContext';
-import { useRouter } from 'next/navigation';
+// import { useUserForm } from '@/context/UserFormContext';
+// import { useRouter } from 'next/navigation';
 import React from 'react'
 
 const DummyJsonProducts = () => {
-  const {userInfo} = useUserForm();
-  const router = useRouter();
+  // const {userInfo} = useUserForm();
+  // const router = useRouter();
   
-  if(!userInfo) router.push('/users/auth/register');
+  // if(!userInfo) router.push('/users/auth/register');
 
   return (
     <>
