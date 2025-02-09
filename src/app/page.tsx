f27e5b36924fe02c8dd2a7e0658c8c15051b777f
chivicoko <victor.c.okoye@gmail.com>
2025-02-09 04:18:05 +0100

    update authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,31 @@ import Categories from "@/components/Categories";
 import Footer from "@/components/footer/Footer";
 import Header from "@/components/Header";
 import Products from "@/components/Products";
-// import { useUserForm } from "@/context/UserFormContext";
-// import { useRouter } from "next/navigation";
+import { useUserForm } from "@/context/UserFormContext";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import Loading from "./loading";
 
 
 const HomePage: React.FC = () => {
-  // const {userInfo} = useUserForm();
-  // const router = useRouter();
-  // console.log(userInfo);
+  const [loading, setLoading] = useState(true);
+
+  const {userInfo} = useUserForm();
+  const router = useRouter();
+  console.log(userInfo);
   
-  // if(!userInfo) router.push('/users/auth/register');
+  useEffect(() => {
+    if (userInfo === null) {
+      setLoading(true);
+    } else {
+      setLoading(false);
+      if(!userInfo) router.push('/users/auth/register');
+    }
+  }, [userInfo, router]);
+
+  if (loading) {
+    return <Loading/>;
+  }
 
   return (
     <div className="">
